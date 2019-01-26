93cee8816ccdb701188962fb2de88f783ddc4271
WZQYANG <39617501+WZQYANG@users.noreply.github.com>
2019-01-26 22:22:15 +0800

    Create pratic_func.h

---
+++ pratic_func.h
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+//STRING
+void push_back_self(const string &cs1,string &s1);
+void toupper_self(string &s1);
+void nonempty_lower_upper(string &s1);
+void os_upp_low_bla(string &s1);
+void os_dig1(string &s1);
+
+
+//VECTOR<INT>
+void add_sum_one_to_hundred(vector<int> &v1);
+void mult_i(vector<int> &v1);
+void os_sum1_sum2_dig2(vector<int> &v1);
+void os_change_order(vector<int> &v1);
+void os_dig3(vector<int> &v1);
+void os_order_question(vector<int> &v1);;
+
+
+//VECTOR<STRING>
+void os_push_back_self(const string &vs, vector<string> &vs1);
+
+
+//VECTOR<CHAR>
+void os_vector_char(const string &vs, vector<char> &vc1);
+
