090206db3a2253e741abf923ac3d6ab6d8f4caf5
WZQYANG <39617501+WZQYANG@users.noreply.github.com>
2019-02-11 16:53:14 +0800

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,16 +1,126 @@
 #pragma once
 
+#ifndef PERSON_H
+#define PERSON_H
+
 #include "pch.h"
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
 #include <vector>
 #include <set>
 #include <map>
+#include <iterator>
 #include <algorithm>
-#include "pratic_func.h"
-
+  
 using namespace std;
 
+/*
+	总体要求：
+
+	1、从输入中读取出name、address和address_price，构建multimap<string,map<string,int>>.
+	2、统计出每个name对应的所有address（set<string>) 、address_count(int)、每个name对应的最高(int)、最低的address值(int)、最后是所有address的总值(int)。
+	3、打印出每一个name对应的所有有效数据。
+
+	构建过程：
+
+	1、一个Person_Infor类，用于构建并保存数据 ：multimap<string,map<string,int>>。
+	2、一个Person_Calculate类，用于读取Person_Infor中的数据并计算出最高和最低的address_price值，将值保存到下一个Person_Result类。
+	3、一个Person_Result类，保存address（set<string>）、address_count(int)、每个name对应的最高(int)、最低的address值(int)、最后是所有address的总值(int)。
+		并构建一个map<set<string>,vector<int>>.
+
+	Person_Infor ps_infor_total(ifstream &infile);
+	Person_Calculate ps_cal_total(ps_infor_total.multimap);
+	Person_Result ps_res(ps_cal_total);
+	print(cout,ps_res);
+
+
+
+	总体要求：
+
+	1、从输入中读取出name、address，构建multimap<string,string>.
+	2、统计出每个name对应的所有address（set<string>).
+	3、打印出每一个name对应的所有有效数据。
+
+	构建过程：
+
+	1、一个Person_Infor类，用于构建并保存数据 ：multimap<string,string>.
+	2、一个Person_Calculate类，用于构建一个set<string>,然后构建一个map<string,set<string>>.
+	3、打印。
+
+	Person_Infor ps_infor_total(istream &infile);
+	Person_Calculate ps_cal_total(ps_infor_total);
+	cout<<ps_cal_total;
+
+*/
+
+class Person_Calculate;
+
+class Person_Infor
+{
+	friend class Person_Calculate;
+public:
+	Person_Infor() = default;
+	Person_Infor(ifstream &is) :name(), address(), two_string_multimap()
+	{
+		string ex;
+		while (getline(is, ex)) {
+			if(ex != "over") {
+				istringstream line(ex);
+				line >> name;
+				line >> address;
+				two_string_multimap.insert({ name, address });// 向map添加元素 《cpp primer》p384
+			}
+		}
+	}
+	Person_Infor(const Person_Infor &) = delete;
+	Person_Infor(Person_Infor &&) = delete;
+	multimap<string, string> map() { return two_string_multimap; }
+	~Person_Infor() = default;
+private:
+	string name;
+	string address;
+	multimap<string, string> two_string_multimap;
+};
+
+
+
+class Person_Calculate
+{
+	friend ostream &print(ostream &,const Person_Calculate &);
+public:
+	Person_Calculate() = default;
+	Person_Calculate(Person_Infor &p_i) :name(), address(), address_set(), name_string_set()
+	{
+		auto goto_map = p_i.map();
+		auto search_person = goto_map.begin();
+		auto search = search_person->first;
+		auto beg = goto_map.lower_bound(search), end = goto_map.upper_bound(search);
+		for (; beg != goto_map.end();) {
+			for (; beg != end; ++beg) {
+				address_set.insert({ beg->second });
+			}
+			--beg;
+			name_string_set.insert({ beg->first, address_set });
+			address_set.erase(address_set.begin(), address_set.end());
+			if (end != goto_map.end()) {
+				search = end->first;
+				beg = goto_map.lower_bound(search);
+				end = goto_map.upper_bound(search);
+			}
+			else
+				break;
+		}
+	}
+	~Person_Calculate() = default;
+private:
+	string name;
+	string address;
+	set<string> address_set;
+	map<string, set<string>> name_string_set;
+};
+ostream &print(ostream &,const Person_Calculate &);
 
 /*
 class Person{
@@ -128,104 +238,12 @@ inline void Person::change_the_most_exp_low_value_address_price(const Person & p
 	the_low_price = ps1.address_price;
 	}
 }
-
 */
 
+#endif //!PERSON_H
 
 
 
-
-
-/*
-	总体要求：
-
-	1、从输入中读取出name、address和address_price，构建multimap<string,map<string,int>>.
-	2、统计出每个name对应的所有address（set<string>) 、address_count(int)、每个name对应的最高(int)、最低的address值(int)、最后是所有address的总值(int)。
-	3、打印出每一个name对应的所有有效数据。
-
-	构建过程：
-
-	1、一个Person_Infor类，用于构建并保存数据 ：multimap<string,map<string,int>>。
-	2、一个Person_Calculate类，用于读取Person_Infor中的数据并计算出最高和最低的address_price值，将值保存到下一个Person_Result类。
-	3、一个Person_Result类，保存address（set<string>）、address_count(int)、每个name对应的最高(int)、最低的address值(int)、最后是所有address的总值(int)。
-		并构建一个map<set<string>,vector<int>>.
-
-	Person_Infor ps_infor_total(ifstream &infile);
-	Person_Calculate ps_cal_total(ps_infor_total.multimap);
-	Person_Result ps_res(ps_cal_total);
-	print(cout,ps_res);
-
-
-
-	总体要求：
-
-	1、从输入中读取出name、address，构建multimap<string,string>.
-	2、统计出每个name对应的所有address（set<string>).
-	3、打印出每一个name对应的所有有效数据。
-
-	构建过程：
-
-	1、一个Person_Infor类，用于构建并保存数据 ：multimap<string,string>.
-	2、一个Person_Calculate类，用于构建一个set<string>,然后构建一个map<string,set<string>>.
-	3、打印。
-
-	Person_Infor ps_infor_total(istream &infile);
-	Person_Calculate ps_cal_total(ps_infor_total);
-	print(cout,ps_cal_total);
-
-*/
-
-class Person_Infor
-{
-	friend Person_Calculate;
-public:
-	Person_Infor() = delete;
-	Person_Infor(istream &infile);
-	Person_Infor(const Person_Infor &) = delete;
-	Person_Infor(Person_Infor &&) = delete;
-	multimap<string,string> map() const { return two_string_multimap; }
-	~Person_Infor() = default;
-private:
-	string name;
-	string address;
-	multimap<string, string> two_string_multimap;
-};
-
-Person_Infor::Person_Infor(istream &is) :name(),address(),two_string_multimap()
-{
-	string ex;
-	while (getline(is,ex)) {
-		while (ex != "over") {
-			stringstream line(ex);
-			line >> name;
-			line >> address;
-			two_string_multimap.insert(name, address);
-		}
-	}
-}
-
-
-class Person_Calculate
-{
-public:
-	Person_Calculate() = default;
-	Person_Calculate(const Person_Infor &p_i);
-	~Person_Calculate() = default;
-private:
-	string name;
-	string address;
-	set<string> address_set;
-};
-Person_Calculate::Person_Calculate(const Person_Infor &p_i) :name(), address(), address_set()
-{
-	auto goto_map = p_i.map();
-	auto goal = goto_map.begin();
-	++goal;
-	for (; goal != goto_map.end();) {
-		auto goal_key = goal->first;
-	}
-
-}
 
 
 
