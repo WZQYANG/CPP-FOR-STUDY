fc34f15271d1a802c60cf740dce0b6539c793914
WZQYANG <39617501+WZQYANG@users.noreply.github.com>
2019-02-13 13:54:49 +0800

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -1,8 +1,24 @@
 #include "pch.h"
 #include "Person.h"
 
+//class  Person_Infor 
 
-ostream  &print(ostream &infor,const Person_Calculate &ps1)
+ifstream &read(ifstream &is, Person_Infor &p_i)
+{
+	string ex;
+	while (getline(is, ex)) {
+		istringstream line(ex);
+		line >> p_i.name >> p_i.address;
+		p_i.two_string_multimap.insert(pair<string, string>(p_i.name, p_i.address));// 向map添加元素 《cpp primer》p384
+	}
+	return is;
+}
+
+
+
+//class Person_Calculate
+
+ostream  &operator<<(ostream &infor,const Person_Calculate &ps1)
 {
 	for (auto print_infor : ps1.name_string_set) {
 		auto print_name = print_infor.first;
